58e0b331eb5cd3fbb5319f7a8cdfff3f690e9c8c
k-madelyn <322960825+k-madelyn@users.noreply.github.com>
2026-09-04 16:59:53 -0500

    add revenue contribution by RFM customer segment calculation file

---
+++ sql/06_revenue_customer_segment.sql
@@ -0,0 +1,35 @@
+WITH rfm_metrics AS (
+    SELECT
+        COUNT(*) AS frequency,
+        SUM(price) AS monetary,
+        MAX(event_time) AS last_purchase_date
+    FROM events
+    WHERE event_type = 'purchase'
+        AND zero_price_product = False
+        AND negative_price = False
+    GROUP BY user_id
+),
+
+recency_date AS (
+    SELECT
+        ((SELECT MAX(event_time)::DATE FROM events) - last_purchase_date) AS recency_days_check,
+        frequency, monetary
+    FROM rfm_metrics
+),
+
+rfm_split AS (
+    SELECT
+        NTILE(4) OVER (ORDER BY recency_days_check DESC) AS r_tier,
+        NTILE(4) OVER (ORDER BY frequency ASC) AS f_tier,
+        NTILE(4) OVER (ORDER BY monetary ASC) AS m_tier,
+        monetary
+    FROM recency_date
+)
+
+SELECT 
+    r_tier || '-' || f_tier || '-' || m_tier AS rfm_tiers,
+    COUNT(*) AS num_of_customers,
+    ROUND(SUM(monetary::NUMERIC), 2) AS total_revenue
+FROM rfm_split
+GROUP BY r_tier, f_tier, m_tier
+ORDER BY total_revenue DESC;
